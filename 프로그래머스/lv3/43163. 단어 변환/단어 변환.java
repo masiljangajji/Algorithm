f12260d01a51cab16cea012108cf7edce8065db3
masiljangajji <hjk172262@gmail.com>
2023-07-12 10:30:01 +0900

    [level 3] Title: 단어 변환, Time: 6.85 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Solution {
+    
+    static boolean []visit = new boolean[51];
+
+    static int Min=123456789;
+    
+    
+    public  boolean go(String str,String[] words,int idx){
+
+        int cnt=0;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)!=words[idx].charAt(i))
+                cnt++;
+        }
+
+        return (cnt==1)?true:false;
+
+    }
+
+    public  void dfs(String str,String target, String[]words,int cnt) {
+
+        if(str.equals(target)) {
+            System.out.println("이제 같네요" +cnt);
+            Min=Math.min(Min,cnt);
+        }
+
+        for (int i = 0; i < words.length; i++) {
+
+            if (visit[i] == true)
+                continue;
+
+            if (go(str, words,i) == true) {
+                visit[i] = true;
+                dfs(words[i],target,words, cnt+1);
+                visit[i]=false;
+            }
+        }
+
+        return;
+
+    }
+    
+    
+    public int solution(String begin, String target, String[] words) {
+        
+        
+        for (int i = 0; i < words.length; i++) {
+            Arrays.fill(visit,false);
+            if(go(begin,words,i)==true){
+                visit[i]=true;
+                System.out.println(words[i]+" "+i+" 아라");
+                dfs(words[i], target,words,1);
+            }
+            System.out.println(Min);
+        }
+
+        System.out.println(Min+" 이거랍니다\n");
+        
+        if(Min==123456789)
+            return 0;
+        else
+            return Min;
+        
+    }
+}
