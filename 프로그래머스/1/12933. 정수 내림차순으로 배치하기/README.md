1b6a671dbbc66775c66d4cec2b8d29ec81f1a342
이승재 <xmfpdlsj0508@gmail.com>
2024-09-29 14:49:16 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 3.58 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 정수 내림차순으로 배치하기 - 12933 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933) 
 
 ### 성능 요약
 
-메모리: 3.59 MB, 시간: 0.01 ms
+메모리: 3.58 MB, 시간: 0.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 28일 18:39:47
+2024년 09월 29일 14:49:14
 
 ### 문제 설명
 
--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -1,22 +1,29 @@
 #include <string>
 #include <vector>
-#include<iostream>
-
+#include <algorithm>
 using namespace std;
+
+bool cmp(int a,int b){
+    return a>b;
+}
 
 long long solution(long long n) {
     long long answer = 0;
     
     vector<int>v;
     
-    while(n>0){
+    while(n){
         v.push_back(n%10);
         n/=10;
-
-        cout<<n<<" ";
     }
     
+    sort(v.begin(),v.end(),cmp);
     
+    for(int i=0;i<v.size()-1;i++){
+        answer+=v[i];
+        answer*=10;
+    }
+    answer+=v[v.size()-1];
     
     return answer;
 }
