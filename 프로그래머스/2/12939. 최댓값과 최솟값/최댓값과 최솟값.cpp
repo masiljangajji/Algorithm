6721691cfb951b5dbb1519d15379337b84edfd44
이승재 <xmfpdlsj0508@gmail.com>
2024-09-29 15:33:56 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    
+    vector<int>v;
+    
+    string str="";
+    for(int i=0;i<s.size();i++){
+        
+    
+        if(s[i]==' '){
+            v.push_back(stoi(str));
+            str="";
+        }
+        else{
+            str+=s[i];    
+        }
+    }
+    
+    v.push_back(stoi(str));
+    
+    sort(v.begin(),v.end());
+    
+    answer+=to_string(v[0]);
+    answer+=" ";
+    answer+=to_string(v[v.size()-1]);
+    
+    return answer;
+}
