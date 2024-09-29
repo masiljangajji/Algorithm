8ff5e687bcb605fe49899740891b914dc9cc25b5
이승재 <xmfpdlsj0508@gmail.com>
2024-09-29 12:53:10 +0900

    [level 1] Title: 내적, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+
+    int ans=0;
+    
+    for(int i=0;i<a.size();i++){
+        ans+=a[i]*b[i];
+    }
+    
+    return ans;
+}
