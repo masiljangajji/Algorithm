aa69551a5bf0cba35c20b58b754b2bbe42d716b2
이승재 <xmfpdlsj0508@gmail.com>
2024-09-29 14:51:05 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.cpp
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.cpp
@@ -4,25 +4,17 @@ using namespace std;
 
 bool solution(string s)
 {
-    bool answer = true;
-
-    // p,y 개수 같으면 true 아니면 false    py하나도 없으면 항상 true 
     
-    int p_cnt=0;
-    int y_cnt=0;
+    int cnt1=0;
+    int cnt2=0;
     
     for(int i=0;i<s.size();i++){
-        if(s[i]=='p'||s[i]=='P')
-            p_cnt++;
-        if(s[i]=='y'||s[i]=='Y')
-            y_cnt++;
+        if(s[i]=='p'||s[i]=='P'){
+            cnt1++;
+        }else if(s[i]=='y'||s[i]=='Y'){
+            cnt2++;
+        }
     }
-    
-    if(p_cnt==0&&y_cnt==0)
-        return true;
-    else if(p_cnt==y_cnt)
-        return true;
-    else
-        return false;
-    
+
+    return cnt1==cnt2;
 }
