9205893c09ed463aadfae8e72cdfb24236edfb4e
이승재 <xmfpdlsj0508@gmail.com>
2025-07-14 16:17:48 +0900

    [Silver II] Title: 최소 힙, Time: 1696 ms, Memory: 104432 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.beans.Introspector;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.*;
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+
+            if (x == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
+                }
+            } else {
+                pq.offer(x);
+            }
+
+        }
+
+    }
+
+
+}
+
+
+
+
+
+
+
