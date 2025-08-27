0d79b2a11f3000973fee870d001a48e19672dd42
이승재 <xmfpdlsj0508@gmail.com>
2025-08-27 17:04:15 +0900

    [Bronze V] Title: A+B, Time: 184 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(sc.nextInt()+sc.nextInt());
+
+    }
+
+}
+
+
