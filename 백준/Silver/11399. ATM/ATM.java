a76cde7059b2b143b51ec6b61590c02c2c45a4f4
이승재 <xmfpdlsj0508@gmail.com>
2025-08-27 16:32:29 +0900

    [Silver IV] Title: ATM, Time: 124 ms, Memory: 16300 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,46 +1,48 @@
-import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.lang.*;
 
 public class Main {
 
-    public static Scanner sc = new Scanner(System.in);
+    public static int answer = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        // 사람은 1번부터 N번까지 번호가 매겨져 있다
-        // i 번 사람이 인출하는데 걸리는 시간 P[i]
+        Scanner sc = new Scanner(System.in);
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 3,1,4,3,2
-        // 3+4+8+11+13 = 39
-        // 1 2 3 4 5 순서
+        // 사람이 줄 서있다
+        // 1번부터 N번까지 있다
 
-        // 2 5 1 4 3 순서
-        // 1,2,3,3,4
-        // 1+3+6+9+13
+        // i번 사람이 인출하는데 걸리는 시간은 Pi 분이다.
 
         int n = sc.nextInt();
+
         int[] arr = new int[n];
+
         for (int i = 0; i < n; i++) {
-            int num = sc.nextInt();
-            arr[i] = num;
+            arr[i] = sc.nextInt();
         }
 
         Arrays.sort(arr);
 
-        int sum = 0;
-        int total = 0;
+        // 1 2 3 3 4
 
-        for (int i = 0; i < n; i++) {
+        // 1 2 3 3 4
+
+        int sum = 0;
+        int answer = 0;
+
+        for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
-            total += sum;
+            answer += sum;
         }
 
-        System.out.println(total);
+        System.out.println(answer);
 
     }
-
 
 }
 
