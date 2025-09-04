835abcd56a77ce31dc9b99566daf6d26af9dc2d7
이승재 <xmfpdlsj0508@gmail.com>
2025-09-04 22:36:29 +0900

    [Silver III] Title: 1로 만들기, Time: 140 ms, Memory: 26000 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // X가 3으로 나누어 떨어지면, 3으로 나눈다.
+
+        int n = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+
+        // 3가지 선택지로 1로 만드는걸 하고있다
+        // 그래서 선택지에 따른 분기가 많아짐
+        // 역으로 가면 최적의 경로만 남음 , 1을 더하거나, 2를 곱하거나, 3을 곱해서 숫자 n을 만들겠다
+
+        int[] dp = new int[1000001 * 3];
+
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 1] + 1, dp[i]);
+            dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1);
+            dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1);
+        }
+
+
+        System.out.println(dp[n]);
+
+
+    }
+
+}
