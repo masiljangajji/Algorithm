9a2e1df9ca2045238b381f8e8d0d54db64a2f348
이승재 <xmfpdlsj0508@gmail.com>
2025-09-04 21:37:16 +0900

    [Bronze II] Title: 피보나치 수, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Long[] fibo = new Long[n + 2];
+
+        fibo[0] = 0L;
+        fibo[1] = 1L;
+        fibo[2] = 1L;
+
+        for (int i = 3; i <= n; i++) {
+            fibo[i] = fibo[i - 1] + fibo[i - 2];
+        }
+
+        System.out.println(fibo[n]);
+    }
+
+}
