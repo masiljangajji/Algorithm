58aa5d2d62c4d3b06e227b279218856e5555fd06
이승재 <xmfpdlsj0508@gmail.com>
2025-09-04 22:38:01 +0900

    [Silver III] Title: 1로 만들기, Time: 128 ms, Memory: 18148 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -20,18 +20,22 @@ public class Main {
         // 그래서 선택지에 따른 분기가 많아짐
         // 역으로 가면 최적의 경로만 남음 , 1을 더하거나, 2를 곱하거나, 3을 곱해서 숫자 n을 만들겠다
 
-        int[] dp = new int[1000001 * 3];
-
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        int[] dp = new int[1000001];
 
         dp[1] = 0;
         dp[2] = 1;
         dp[3] = 1;
 
-        for (int i = 2; i <= n; i++) {
-            dp[i] = Math.min(dp[i - 1] + 1, dp[i]);
-            dp[i * 2] = Math.min(dp[i * 2], dp[i] + 1);
-            dp[i * 3] = Math.min(dp[i * 3], dp[i] + 1);
+        for (int i = 4; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
         }
 
 
