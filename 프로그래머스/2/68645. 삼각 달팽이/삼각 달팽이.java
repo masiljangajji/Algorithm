fec63dcf4279d67a13683f266bbbe64303d2ee18
이승재 <xmfpdlsj0508@gmail.com>
2025-10-03 16:05:29 +0900

    [level 2] Title: 삼각 달팽이, Time: 10.91 ms, Memory: 123 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -6,61 +6,80 @@ class Solution {
     public int[] solution(int n) {
         
         
-        int size=n;
+        int cnt = 1;
+        int number = n;
         
-        int [][] arr = new int[n][n];
+    
+        int [][] arr= new int[n][n];
+                
+        int col = 0;
+        int row = 0;
+        int count =0;
         
-        int cnt=1;
-        
-        int y=0,x=0;
-        int dir=1;
-        
-        while(n>0){
-            
-            if(dir==1){
-                for(int i=0;i<n;i++){
-                    arr[x][y++] = cnt++;
-                }            
-
-                y--;
-                x++;
-                n--;  
-                dir++;
-                continue;
-            }
-            
-            if(dir ==2){
-                for(int i=0;i<n;i++){
-                    arr[x++][y]=cnt++;
-                }
-
-                x--;
-                n--;                
-                dir++;
-                continue;
-            }
-            
+        while(true){
             
             for(int i=0;i<n;i++){
-                arr[--x][--y]=cnt++;
+                arr[col++][row] = cnt++;
             }
-        
-            y++;
+                        
+            row++;
+            col--;
             n--;
-            dir=1;
             
-        }
-        
-        int [] answer= new int[cnt-1];        
-        int cnt2=0;
-        for(int i=0;i<size;i++){
-            for(int j=0;j<=i;j++){
-                answer[cnt2++]= arr[j][i];
+            if(n==0){
+                break;
             }
-            System.out.println();
+            
+            for(int i=0;i<n;i++){
+                arr[col][row++] = cnt++;
+            }
+
+            row--;
+            n--;
+            
+            if(n==0){
+                break;
+            }
+            
+            for(int i=0;i<n;i++){
+                arr[--col][--row] = cnt++;
+            }   
+            col++;
+            n--;
+            
+            if(n==0){
+                break;
+            }
+        
         }
         
+        int []answer = new int[cnt-1];
+        
+        int idx = 0;
+        
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if(arr[i][j]!=0){
+                    answer[idx++] = arr[i][j];
+                }else
+                    break;
+            }
+        }
         
         return answer;
     }
+    
+    public void prt(int[][]arr){
+        
+        System.out.println("Start");
+
+        
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.printf("%3d ",arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    
 }
