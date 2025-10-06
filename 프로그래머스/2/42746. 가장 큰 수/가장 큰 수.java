af7f4c84e54b685ee2e34e3d756b4eff7dfd7afe
이승재 <xmfpdlsj0508@gmail.com>
2025-10-06 20:48:34 +0900

    [level 2] Title: 가장 큰 수, Time: 242.64 ms, Memory: 147 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    
+    public String solution(int[] numbers) {
+
+        return Arrays.stream(numbers)
+            .mapToObj(String::valueOf)
+            .sorted((v1,v2)->{
+            int original = Integer.parseInt(v1+v2);
+            int reverse = Integer.parseInt(v2+v1);
+            return reverse - original;
+        }).collect(Collectors.joining(""))
+            .replaceAll("^0+","0");
+ 
+    }
+}
+
+
