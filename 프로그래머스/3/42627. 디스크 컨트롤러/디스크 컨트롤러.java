68ea652bc43c63cb76853e5e149162c6f677a802
이승재 <xmfpdlsj0508@gmail.com>
2025-11-04 04:51:30 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.41 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,60 +2,70 @@ import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
         
+        
         Arrays.sort(jobs,(v1,v2)->{
             return v1[0]-v2[0];
         });
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>(
+        PriorityQueue<Job>pq= new PriorityQueue<>(
             (v1,v2)->{
-            if(v1.duration==v2.duration){
-                if(v1.start==v2.start){
-                    return v1.idx-v2.idx;
+                if(v1.duration==v2.duration){
+                    if(v1.time==v2.time)
+                        return v1.number-v2.number;
+                    return v1.time-v2.time;
                 }
-                return v1.start-v2.start;
-            }
-            return v1.duration-v2.duration;
+                return v1.duration-v2.duration;
             }
         );
-            
         
         int time=0;
+        int i=0;
         int answer=0;
-        int idx=0;
-        int done=0;
+        int n = jobs.length;
         
-        while(done<jobs.length){
+        
+        while(true){
             
-            while(idx<jobs.length&&time>=jobs[idx][0]){
-                pq.add(new Pair(idx,jobs[idx][0],jobs[idx][1]));
-                idx++;
+            if(i>=n&&pq.isEmpty())
+                return answer/n;
+            
+            // 현재 시각까지 도착한 작업 전부 더하기
+            while(i<n&&jobs[i][0]<=time){
+                pq.add(new Job(i,jobs[i][0],jobs[i][1]));
+                i++;
             }
-           
-            if(!pq.isEmpty()){
-                Pair p = pq.poll();
-                time+=p.duration;
-                answer+=(time-p.start);
-                done++;
-            }else{
-                time = Math.max(time,jobs[idx][0]);
+            
+            if(i<jobs.length&&pq.isEmpty()){
+                pq.add(new Job(i,jobs[i][0],jobs[i][1]));
+                time = jobs[i][0];
+                i++;
+                continue;
             }
+            
+            Job j = pq.poll();
+
+            while(i<jobs.length&&j.duration+time>=jobs[i][0]){
+                pq.add(new Job(i,jobs[i][0],jobs[i][1]));
+                i++;
+            }
+            
+            time+=j.duration;
+            answer+=(time-j.time);
+            
         }
         
-        return answer/jobs.length;
     }
 }
 
-class Pair{
+class Job{
     
-    public int idx;
+    int number;
+    int time;
+    int duration;
     
-    public int start;
-    
-    public int duration;
-    
-    public Pair(int idx,int start,int duration){
-        this.idx = idx;
-        this.start = start;
+    Job(int number,int time,int duration){
+        this.number =number;
+        this.time = time;
         this.duration = duration;
     }
     
